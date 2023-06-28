07929a3fd07ac6aac3748d2b65866bc4e40da762
Deivisson Lisboa <dvilaflor@gmail.com>
2023-06-28 16:40:02 -0300

    Implement fileanalyse endpoint

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express")
+var cors = require("cors")
+require("dotenv").config()
+const multer = require("multer")
+const maxFileSize = 10 * 10 ** 6 // 10 MB
+const upload = multer({ dest: "uploads/", limits: { fileSize: maxFileSize } })
+const fs = require("fs")
+const { promisify } = require("util")
+const unlinkAsync = promisify(fs.unlink)
 
-var app = express();
+const fileSizeLimitHandler = (err, req, res, next) => {
+  if (err) {
+    res.sendStatus(413)
+  } else {
+    next()
+  }
+}
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+var app = express()
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+app.use(cors())
+app.use("/public", express.static(process.cwd() + "/public"))
 
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html")
+})
 
+app.post(
+  "/api/fileanalyse",
+  upload.single("upfile"),
+  fileSizeLimitHandler,
+  async (req, res) => {
+    const { originalname, mimetype, size, path } = req.file
 
+    await unlinkAsync(path)
 
-const port = process.env.PORT || 3000;
+    res.json({
+      name: originalname,
+      type: mimetype,
+      size,
+    })
+  }
+)
+
+const port = process.env.PORT || 3000
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-});
+  console.log("Your app is listening on port " + port)
+})
